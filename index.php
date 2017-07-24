9dabee1bfd5c5a442e580cec7a58ceb6c5198d05
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-07-25 01:24:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <title>Inny tytuł33333</title>
 </head>
-<body style="background-color: yellow;">
+<body style="background-color: green;">
   <h1>Przykładowa strona</h1>
   <h2>Coś tam coś tam</h2>
 </body>
